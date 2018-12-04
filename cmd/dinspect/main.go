af2cdc0e6a77af83c44b07afdd3325e96be15eb6
Murat Kabilov <murat@kabilov.com>
2018-12-04 17:22:09 +0100

    dinspect: inspect multiple deltas

--- cmd/dinspect/main.go
+++ cmd/dinspect/main.go
@@ -12,7 +12,7 @@ import (
 )
 
 var (
-	filePath string
+	filePaths []string
 
 	Version  string
 	Revision string
@@ -31,50 +31,48 @@ func init() {
 		os.Exit(1)
 	}
 
-	filePath = os.Args[1]
-	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		fmt.Fprintf(os.Stderr, "files %q does not exist", filePath)
-		os.Exit(1)
-	}
+	filePaths = os.Args[1:]
 }
 
 func main() {
-	fmt.Printf("reading %q segment file: \n\n", filePath)
+	for _, filePath := range filePaths {
+		fmt.Printf("reading %q segment file: \n\n", filePath)
 
-	fp, err := os.OpenFile(filePath, os.O_RDONLY, os.ModePerm)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "could not open file: %v\n", err)
-		os.Exit(1)
-	}
-	defer fp.Close()
-
-	lnBuf := make([]byte, 8)
-	for {
-		if n, err := fp.Read(lnBuf); err == io.EOF && n == 0 {
-			break
-		} else if err != nil || n != 8 {
-			fmt.Fprintf(os.Stderr, "could not read: %v\n", err)
-			os.Exit(1)
-		}
-
-		ln := binary.BigEndian.Uint64(lnBuf) - 8
-		dataBuf := make([]byte, ln)
-
-		if n, err := fp.Read(dataBuf); err == io.EOF && n == 0 {
-			break
-		} else if err != nil || uint64(n) != ln {
-			fmt.Fprintf(os.Stderr, "could not read %d bytes: %v", ln, err)
-			os.Exit(1)
-		}
-
-		msg, err := decoder.Parse(dataBuf)
+		fp, err := os.OpenFile(filePath, os.O_RDONLY, os.ModePerm)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "could not parse message: %v", err)
+			fmt.Fprintf(os.Stderr, "could not open file: %v\n", err)
 			os.Exit(1)
 		}
+		defer fp.Close()
 
-		msgType := msg.MsgType()
-		delimiter := strings.Repeat(" ", 10-len(msgType))
-		fmt.Printf("%s:%s%s\n", msgType, delimiter, msg.String())
+		lnBuf := make([]byte, 8)
+		for {
+			if n, err := fp.Read(lnBuf); err == io.EOF && n == 0 {
+				break
+			} else if err != nil || n != 8 {
+				fmt.Fprintf(os.Stderr, "could not read: %v\n", err)
+				os.Exit(1)
+			}
+
+			ln := binary.BigEndian.Uint64(lnBuf) - 8
+			dataBuf := make([]byte, ln)
+
+			if n, err := fp.Read(dataBuf); err == io.EOF && n == 0 {
+				break
+			} else if err != nil || uint64(n) != ln {
+				fmt.Fprintf(os.Stderr, "could not read %d bytes: %v", ln, err)
+				os.Exit(1)
+			}
+
+			msg, err := decoder.Parse(dataBuf)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "could not parse message: %v", err)
+				os.Exit(1)
+			}
+
+			msgType := msg.MsgType()
+			delimiter := strings.Repeat(" ", 10-len(msgType))
+			fmt.Printf("%s:%s%s\n", msgType, delimiter, msg.String())
+		}
 	}
 }
